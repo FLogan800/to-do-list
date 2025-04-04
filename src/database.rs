c62621f7353873f8bf9bc91a2da2b81b83c73749
Logan Ferris <FLogan800@gmail.com>
2025-04-04 11:34:47 -0500

    Add database functionality for completing and deleting tasks

--- src/database.rs
+++ src/database.rs
@@ -10,6 +10,7 @@ pub fn init() -> Connection {
         id          INTEGER PRIMARY KEY,
         title       TEXT NOT NULL,
         description TEXT
+        complete    BOOLEAN DEFAULT 0
         )",
         (),
     )
@@ -23,6 +24,24 @@ pub fn insert_task(conn: Connection, task: task::Task) {
         "
     INSERT INTO tasks (id, title, description) VALUES (?1, ?2, ?3)",
         (task.id, task.title, task.description),
+    )
+    .unwrap();
+}
+
+pub fn complete_task(conn: Connection, id: i32) {
+    conn.execute(
+        "
+        UPDATE tasks SET complete = 1 WHERE id = ?1",
+        (id,),
+    )
+    .unwrap();
+}
+
+pub fn delete_task(conn: Connection, id: i32) {
+    conn.execute(
+        "
+    DELETE tasks WEHRE id = ?1",
+        (id,),
     )
     .unwrap();
 }
--- src/task.rs
+++ src/task.rs
@@ -2,4 +2,5 @@ pub struct Task {
     pub id: i32,
     pub title: String,
     pub description: Option<String>,
+    pub complete: bool,
 }
